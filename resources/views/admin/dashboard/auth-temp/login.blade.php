6f9308a321a7d605e6521cff077bbd66966760fd
unknown <eliseevv02@mail.ru>
2022-07-15 18:13:36 +0300

    chore: remove unnecessary items

--- resources/views/admin/dashboard/auth-temp/login.blade.php
+++ resources/views/admin/dashboard/auth-temp/login.blade.php
@@ -54,21 +54,7 @@
                         <button class="btn btn-primary px-4" type="submit">{{ __('Login') }}</button>
                     </div>
                     </form>
-                    <div class="col-6 text-right">
-                        <a href="{{ route('password.request') }}" class="btn btn-link px-0" type="button">{{ __('Forgot Your Password?') }}</a>
                     </div>
-                    </div>
-              </div>
-            </div>
-            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-              <div class="card-body text-center">
-                <div>
-                  <h2>Sign up</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  @if (Route::has('password.request'))
-                    <a href="{{ route('register') }}" class="btn btn-primary active mt-3" type="button">{{ __('Register') }}</a>
-                  @endif
-                </div>
               </div>
             </div>
           </div>
