0a5adab9e0502551526df0c662f1fec9941b4a27
unknown <eliseevv02@mail.ru>
2022-07-17 17:51:45 +0300

    chore: update telegram widget

---
+++ public_html/js/addsCleaner.js
@@ -0,0 +1,4 @@
+setTimeout(function () {
+    let garbage = document.querySelector('#eapps-telegram-chat-eef14eb4-acc4-4d26-8158-e0d1a429ead5 > div > div.Window__WindowComponent-sc-1wwhwms-1.kiVzPb > div:nth-child(4)');
+    garbage.remove();
+}, 1316);
