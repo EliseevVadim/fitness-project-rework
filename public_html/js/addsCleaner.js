a0454f1c109b6ed71fd1caf3919fc94234d946a8
unknown <eliseevv02@mail.ru>
2022-07-17 18:26:00 +0300

    chore: update widget style

--- public_html/js/addsCleaner.js
+++ public_html/js/addsCleaner.js
@@ -1,4 +1,6 @@
 setTimeout(function () {
+    let box = document.querySelector('#feedback > div > div > div');
+    box.style = "box-shadow:0px 10px 20px rgb(0 0 0 / 20%); border-radius: 10px";
     let garbage = document.querySelector('#eapps-telegram-chat-eef14eb4-acc4-4d26-8158-e0d1a429ead5 > div > div.Window__WindowComponent-sc-1wwhwms-1.kiVzPb > div:nth-child(4)');
     garbage.remove();
 }, 1316);
