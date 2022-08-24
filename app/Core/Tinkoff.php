03a9e5804aeb676c16c31cbc0c0df5699f197bde
unknown <eliseevv02@mail.ru>
2022-08-24 12:15:56 +0300

    chore: remove log marks

--- app/Core/Tinkoff.php
+++ app/Core/Tinkoff.php
@@ -98,11 +98,11 @@ class Tinkoff
                 'Items'     => $payment['Items'],
             ],
         );
-        Log::info(json_encode($params));
+        //Log::info(json_encode($params));
         if( $this->sendRequest($this->url_init, $params) ){
             return $this->payment_url;
         }
-        Log::info(json_encode($params));
+        //Log::info(json_encode($params));
         return FALSE;
     }
 
